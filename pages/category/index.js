8310dc8e84d56f1e805262c2fafc66e94fbf0016
Crystal0504 <www.1426458287@outlook.com>
2019-12-10 06:38:10 +0800

    JavaScript

--- pages/category/index.js
+++ pages/category/index.js
@@ -12,48 +12,50 @@ Page({
    */
   data: {
     //左侧菜单数据
-    leftMenuList:[],
+    leftMenuList: [],
     //商品数据
-    rightContent:[],
+    rightContent: [],
     //被点击的左侧菜单
-    currentIndex:0
+    currentIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.getCates();
   },
 
   //获取分类数据
 
-getCates(){
-  request({
-    url:"https://api.zbztb.cn/api/public/v1/categories"
-  }).then(res=>{
-    // console.log(res);
-    this.Cates=res.data.message;
+  getCates() {
+    request({
+      url: "https://api.zbztb.cn/api/public/v1/categories"
+    }).then(res => {
+      // console.log(res);
+      this.Cates = res.data.message;
 
-    //构建左侧大菜单数据
-    let leftMenuList=this.Cates.map(v=>v.cat_name);
+      //构建左侧大菜单数据
+      let leftMenuList = this.Cates.map(v => v.cat_name);
+      //构造右侧商品数据
+      let rightContent = this.Cates[0].children;
+      this.setData({
+        leftMenuList,
+        rightContent
+      });
+    })
+  },
+  //左侧点击事件
+  handleItemTap(e) {
+    // console.log(e);
+    const {
+      index
+    } = e.currentTarget.dataset;
     //构造右侧商品数据
-    let rightContent=this.Cates[0].children;
+    let rightContent = this.Cates[index].children;
     this.setData({
-      leftMenuList,
+      currentIndex: index,
       rightContent
     });
-  })
-},
-//左侧点击事件
-handleItemTap(e){
-  // console.log(e);
-  const {index}=e.currentTarget.dataset;
-  //构造右侧商品数据
-  let rightContent=this.Cates[index].children;
-  this.setData({
-    currentIndex:index,
-    rightContent
-  });
-}
+  }
 })
