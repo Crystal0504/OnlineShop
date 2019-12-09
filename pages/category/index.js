c21b1b4dbec13c6cf7ad3798b1a3945741780733
Crystal0504 <www.1426458287@outlook.com>
2019-12-10 06:37:16 +0800

    JavaScript

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,59 @@
 // pages/category/index.js
+
+//0.引入用来发送请求的方法
+import {
+  request
+} from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    //左侧菜单数据
+    leftMenuList:[],
+    //商品数据
+    rightContent:[],
+    //被点击的左侧菜单
+    currentIndex:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //获取分类数据
 
-  },
+getCates(){
+  request({
+    url:"https://api.zbztb.cn/api/public/v1/categories"
+  }).then(res=>{
+    // console.log(res);
+    this.Cates=res.data.message;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    //构建左侧大菜单数据
+    let leftMenuList=this.Cates.map(v=>v.cat_name);
+    //构造右侧商品数据
+    let rightContent=this.Cates[0].children;
+    this.setData({
+      leftMenuList,
+      rightContent
+    });
+  })
+},
+//左侧点击事件
+handleItemTap(e){
+  // console.log(e);
+  const {index}=e.currentTarget.dataset;
+  //构造右侧商品数据
+  let rightContent=this.Cates[index].children;
+  this.setData({
+    currentIndex:index,
+    rightContent
+  });
+}
 })
