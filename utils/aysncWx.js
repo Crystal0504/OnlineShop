7ff2b7dcf7ff5c775740286c6ad157314fd91eba
Crystal0504 <www.1426458287@outlook.com>
2020-02-04 16:29:20 +0800

    小程序优化

--- utils/aysncWx.js
+++ utils/aysncWx.js
@@ -39,3 +39,37 @@ export const openSetting = () => {
 
     })
 }
+
+export const showModal = ({
+    content
+}) => {
+    return new Promise((resolve, reject) => {
+        wx.showModal({
+            title: '提示',
+            content: content,
+            success: (res) => {
+                resolve(res);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        });
+    })
+}
+
+export const showToast = ({
+    title
+}) => {
+    return new Promise((resolve, reject) => {
+        wx.showToast({
+            title: title,
+            icon: 'none',
+            success: (res) => {
+                resolve(res);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        });
+    })
+}
