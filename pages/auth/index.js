c9fa21ed2356c6b47424a33d0ff88be4249aee65
Crystal0504 <www.1426458287@outlook.com>
2020-02-07 17:39:25 +0800

    用户信息界面

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,66 +1,36 @@
 // pages/auth/index.js
+import {
+  request
+} from "../../request/index.js";
+
+import regeneratorRuntime from "../../lib/runtime/runtime";
+import { login } from "../../utils/aysncWx.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //获取用户信息
+  async handleGetUserInfo(e){
+    try {
+      // console.log(e);
+    const {encrypteData,rawData,iv,signature} = e.detail;
+    const {code} = await login();
+    const loginParams = {encrypteData,rawData,iv,signature,code}
+    // console.log(code);
+    const {token} = await request({
+      url:"https://api.zbztb.cn/api/public/v1/users/wxlogin",
+      data:loginParams,
+      method:"post"
+    });
+    // console.log(res);
+    wx.setStorageSync("token", token);
+    wx.navigateBack({
+      delta: 1
+    });
+    } catch (error) {
+      console.log(error);
+    }
+      
+      
+    
+    
+      
   }
 })
