17f3e0418e177c6e74f0fa9bae19aa62752e9433
Crystal0504 <www.1426458287@outlook.com>
2020-02-08 17:26:56 +0800

    订单页面

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,66 +1,95 @@
 // pages/order/index.js
+import {
+  request
+} from "../../request/index.js";
+
+import regeneratorRuntime from "../../lib/runtime/runtime";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    orders: [],
+    tabs: [{
+        id: 0,
+        value: "全部",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "待付款",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "待发货",
+        isActive: false
+      },
+      {
+        id: 3,
+        value: "退款/退货",
+        isActive: false
+      }
+    ]
+  },
+  onShow(options) {
+    // console.log(options);
+    let pages = getCurrentPages();
+    // console.log(pages);
+    let currentPage = pages[pages.length - 1];
+    // console.log(currentPage.options);
+    const {
+      type
+    } = currentPage.options;
+    this.changeTitleByIndex(type - 1);
+    this.getOrders(type);
+
+
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  async getOrders(type) {
+    const token = wx.getStorageSync("token");
+    if (!token) {
+      wx.navigateTo({
+        url: '/pages/auth/index'
+      });
+      return;
+    }
+    const header = {
+      Authorization: token
+    };
+    const res = await request({
+      url: "https://api.zbztb.cn/api/public/v1/my/orders/all",
+      data: {
+        type
+      },
+      method: "GET",
+      header
+    });
+    // console.log(res);
+    this.setData({
+      orders: res.orders.map(v => ({
+        ...v,
+        create_time_cn: (new Date(v.create_time * 1000).toLocaleString())
+      }))
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  changeTitleByIndex(index) {
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    this.setData({
+      tabs
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handeleTabsItemChange(e) {
+    // console.log(e);
+    const {
+      index
+    } = e.detail;
+    this.changeTitleByIndex(index);
+    this.getOrders(index + 1);
   }
 })
