281cf97177ee3d970e32305dcc77329fe9a9d75a
Crystal0504 <www.1426458287@outlook.com>
2020-02-10 18:35:44 +0800

    首页

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,10 +1,16 @@
 // pages/user/index.js
 Page({
   data: {
-    userinfo:{}
+    userinfo: {},
+    collectNums: 0
   },
-  onShow(){
+  onShow() {
+    const collect = wx.getStorageSync("collect") || [];
+
     const userinfo = wx.getStorageSync("userinfo");
-    this.setData({userinfo});
+    this.setData({
+      userinfo,
+      collectNums: collect.length
+    });
   }
 })
